e5e426e579da782540453abc2e199219bcb90963
Artem <artemklinkov@yandex.ru>
2021-02-20 19:46:25 +0300

    Helpdesk client

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# ahj_helpdesk_client
+[![Build status](https://ci.appveyor.com/api/projects/status/m1mnjyyrhqaddn51?svg=true)](https://ci.appveyor.com/project/ArtemKlinkov/ahj-helpdesk-client)
+https://artemklinkov.github.io/ahj-helpdesk-client/
---
+++ src/js/InitData.js
@@ -0,0 +1,18 @@
+/* eslint-disable no-console */
+export default function initData() {
+  const params = new URLSearchParams();
+  params.append('name', 'Поменять краску');
+  params.append('description', 'Поменять краску в принтере, ком 404');
+
+  const xhr = new XMLHttpRequest();
+  xhr.open('POST', 'https://ahj-helpdesk-server.herokuapp.com/');
+  xhr.addEventListener('load', () => {
+    if (xhr.status === 200) {
+      console.log(xhr.responseText);
+    } else {
+      // TODO: handle other status
+      console.log(xhr.responseText);
+    }
+  });
+  xhr.send(params);
+}
